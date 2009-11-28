8ff25304730fc80469da23ac4d8ff859e7c7db0f
Matt Vasquez <mvasquez@fellowshiptech.com>
2009-11-28 15:58:37 -0600

    made changes to the home page. added a link to a wufoo contact form. also added an about page that is incomplete.

--- dwarfmondo.rb
+++ dwarfmondo.rb
@@ -4,5 +4,11 @@ require 'sinatra'
 get '/' do
   @page_title = 'Web Application Design + Rails Development'
   
-  erb :index
+  erb :index, {:layout => :index_layout}
+end
+
+get '/about' do
+  @page_title = 'About'
+  
+  erb :about
 end
